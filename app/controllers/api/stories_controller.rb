38bc404c50c9876d94d4c2e2fda100e25f439a1a
tuanminh20 <minhnguyenfu@gmail.com>
2020-01-16 14:02:34 +0700

    add controller stories and api/stories

---
+++ app/controllers/api/stories_controller.rb
@@ -0,0 +1,7 @@
+class Api::StoriesController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  namespace :api do
+    get 'stories/index'
+    get 'stories/show'
+  end
+  root 'stories#index'
+  get 'stories/show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
